a64a99d688d4a8464774889c43a7f3ccb8972b47
Utkarsh <upatel0192@gmail.com>
2024-04-02 23:39:52 +0530

    Test cases

--- test_pet.py
+++ test_pet.py
@@ -2,6 +2,7 @@ from jsonschema import validate
 import pytest
 import schemas
 import api_helpers
+import json
 from hamcrest import assert_that, contains_string, is_
 
 '''
@@ -26,21 +27,30 @@ TODO: Finish this test by...
 3) Validate the 'status' property in the response is equal to the expected status
 4) Validate the schema for each object in the response
 '''
-@pytest.mark.parametrize("status", [("available")])
+@pytest.mark.parametrize("status", [("available"), ("sold"), ("pending")])
 def test_find_by_status_200(status):
     test_endpoint = "/pets/findByStatus"
     params = {
         "status": status
     }
-
     response = api_helpers.get_api_data(test_endpoint, params)
-    # TODO...
+    assert response.status_code == 200
+    for info in response.json():
+        assert info["status"] == status
+        validate(instance=info, schema=schemas.pet) 
+
+    
 
 '''
 TODO: Finish this test by...
 1) Testing and validating the appropriate 404 response for /pets/{pet_id}
 2) Parameterizing the test for any edge cases
 '''
-def test_get_by_id_404():
-    # TODO...
-    pass
+@pytest.mark.parametrize("pet_id", [-1])
+def test_get_by_id_404(pet_id):
+    test_endpoint = f"/pets/{pet_id}"
+    params = {
+        "pet_id": pet_id
+    }
+    response = api_helpers.get_api_data(test_endpoint, params)
+    assert response.status_code == 404
--- test_store.py
+++ test_store.py
@@ -3,7 +3,8 @@ import pytest
 import schemas
 import api_helpers
 from hamcrest import assert_that, contains_string, is_
-
+import json 
+import time
 '''
 TODO: Finish this test by...
 1) Creating a function to test the PATCH request /store/order/{order_id}
@@ -12,5 +13,44 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
-    pass
+
+@pytest.fixture
+def generate_random_json():
+    timestamp_ms = int(time.time() * 1000)
+    name = "string"  
+    type = "cat"     
+    status = "available"  
+    
+    random_json = {
+        "id": timestamp_ms,
+        "name": name,
+        "type": type,
+        "status": status
+    }
+    
+    return json.dumps(random_json)
+
+def test_patch_order_by_id(generate_random_json):
+    create_pet_endpoint = "/pets"
+    create_response = api_helpers.post_api_data(create_pet_endpoint, json.loads(generate_random_json))
+    assert create_response.status_code == 201
+    order_id = store_order(create_response.json().get('id'))
+    patch_endpoint = "/store/order/" + order_id
+    data = {
+          "status": "available"
+    }
+    response = api_helpers.patch_api_data(patch_endpoint, data)
+    assert response.status_code == 200
+    assert response.json().get("message") == 'Order and pet status updated successfully'
+     
+def store_order(pet_id):
+    create_order_endpoint = "/store/order"
+    params = {
+        "pet_id": pet_id
+    }
+    response = api_helpers.post_api_data(create_order_endpoint, params)
+    assert response.status_code == 201
+    if response.status_code == 201:
+        validate(response.json(), schemas.order)
+    return response.json().get('id')
+
